37c613e6f6e81d98b2171bc580ac0edb5235cca3
Yif-Yang <yif_yang@qq.com>
2021-11-23 03:06:24 +0800

    aux 12 runable

--- models/resnet.py
+++ models/resnet.py
@@ -96,7 +96,8 @@ class ResNet(nn.Module):
         self.conv5_x = self._make_layer(block, 512, num_block[3], 2)
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
-
+        self.linear_aux_1 = nn.Linear(512*block.expansion, num_classes)
+        self.linear_aux_2 = nn.Linear(512*block.expansion, num_classes)
     def _make_layer(self, block, out_channels, num_blocks, stride):
         """make resnet layers(by layer i didnt mean this 'layer' was the
         same as a neuron netowork layer, ex. conv layer), one layer may
@@ -130,9 +131,9 @@ class ResNet(nn.Module):
         output = self.conv5_x(output)
         output = self.avg_pool(output)
         output = output.view(output.size(0), -1)
-        output = self.fc(output)
+        ret = self.fc(output), self.linear_aux_1(output.detach()), self.linear_aux_2(output.detach())
 
-        return output
+        return ret
 
 def resnet18():
     """ return a ResNet 18 object
--- train.py
+++ train.py
@@ -25,11 +25,17 @@ from torch.utils.tensorboard import SummaryWriter
 from conf import settings
 from utils import get_network, get_training_dataloader, get_test_dataloader, WarmUpLR, \
     most_recent_folder, most_recent_weights, last_epoch, best_acc_weights
+from torch.nn.functional import softmax
 
-def train(epoch):
+def train(epoch, dis_lambda=1):
 
     start = time.time()
     net.train()
+    train_loss = 0
+    correct = 0
+    correct_aux_1 = 0
+    correct_aux_2 = 0
+    total = 0
     for batch_index, (images, labels) in enumerate(cifar100_training_loader):
 
         if args.gpu:
@@ -38,9 +44,25 @@ def train(epoch):
 
         optimizer.zero_grad()
         outputs = net(images)
-        loss = loss_function(outputs, labels)
+        main_cls_out, aux_1, aux_2 = outputs
+        loss_main = loss_function(main_cls_out, labels)
+        loss_aux1_cls = loss_function(aux_1, labels)
+        loss_aux2_cls = loss_function(aux_2, labels)
+        loss_aux_dis = dis_criterion(softmax(aux_1, 1), softmax(aux_2, 1))
+        loss_main_dis = dis_criterion(softmax(main_cls_out, 1), (softmax(aux_1.detach(), 1) + softmax(aux_2.detach(), 1)) / 2)
+        loss = loss_main + loss_aux1_cls + loss_aux2_cls - loss_aux_dis * dis_lambda + loss_main_dis
         loss.backward()
         optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = main_cls_out.max(1)
+        _, predicted_aux_1 = aux_1.max(1)
+        _, predicted_aux_2= aux_2.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+        correct_aux_1 += predicted_aux_1.eq(labels).sum().item()
+        correct_aux_2 += predicted_aux_2.eq(labels).sum().item()
+
 
         n_iter = (epoch - 1) * len(cifar100_training_loader) + batch_index + 1
 
@@ -51,13 +73,18 @@ def train(epoch):
             if 'bias' in name:
                 writer.add_scalar('LastLayerGradients/grad_norm2_bias', para.grad.norm(), n_iter)
 
-        print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
-            loss.item(),
-            optimizer.param_groups[0]['lr'],
-            epoch=epoch,
-            trained_samples=batch_index * args.b + len(images),
-            total_samples=len(cifar100_training_loader.dataset)
-        ))
+        # print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
+        #     loss.item(),
+        #     optimizer.param_groups[0]['lr'],
+        #     epoch=epoch,
+        #     trained_samples=batch_index * args.b + len(images),
+        #     total_samples=len(cifar100_training_loader.dataset)
+        # ))
+        if batch_index % 100 == 0:
+            print(batch_index, len(cifar100_training_loader), 'LR: %.4f | Train Loss: %.3f | Acc: %.3f%% | Acc aux1: %.3f%% | Acc aux2: %.3f%% (%d/%d)'
+                         % (optimizer.param_groups[0]['lr'], loss / (batch_index + 1), 100. * correct / total, 100. * correct_aux_1 / total,100. * correct_aux_2 / total, correct,
+                            total) )
+            print(f"loss_main:{loss_main:.3f}, loss_aux1_cls:{loss_aux1_cls:.3f}, loss_aux2_cls:{loss_aux2_cls:.3f}, loss_aux_dis:{loss_aux_dis:.5f}, loss_main_dis:{loss_main_dis:.5f}")
 
         #update training loss for each iteration
         writer.add_scalar('Train/loss', loss.item(), n_iter)
@@ -75,7 +102,7 @@ def train(epoch):
     print('epoch {} training time consumed: {:.2f}s'.format(epoch, finish - start))
 
 @torch.no_grad()
-def eval_training(epoch=0, tb=True):
+def eval_training(epoch=0, tb=True, output_idx=0):
 
     start = time.time()
     net.eval()
@@ -90,6 +117,8 @@ def eval_training(epoch=0, tb=True):
             labels = labels.cuda()
 
         outputs = net(images)
+        outputs = outputs[output_idx]
+
         loss = loss_function(outputs, labels)
 
         test_loss += loss.item()
@@ -97,11 +126,12 @@ def eval_training(epoch=0, tb=True):
         correct += preds.eq(labels).sum()
 
     finish = time.time()
-    if args.gpu:
-        print('GPU INFO.....')
-        print(torch.cuda.memory_summary(), end='')
+    # if args.gpu:
+    #     print('GPU INFO.....')
+    #     print(torch.cuda.memory_summary(), end='')
     print('Evaluating Network.....')
-    print('Test set: Epoch: {}, Average loss: {:.4f}, Accuracy: {:.4f}, Time consumed:{:.2f}s'.format(
+    print('output_idx: {} \n Test set: Epoch: {}, Average loss: {:.4f}, Accuracy: {:.4f}, Time consumed:{:.2f}s'.format(
+        output_idx,
         epoch,
         test_loss / len(cifar100_test_loader.dataset),
         correct.float() / len(cifar100_test_loader.dataset),
@@ -120,15 +150,16 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-net', type=str, required=True, help='net type')
-    parser.add_argument('-gpu', action='store_true', default=False, help='use gpu or not')
+    parser.add_argument('-gpu', action='store_true', default=True, help='use gpu or not')
     parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
     parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
     parser.add_argument('-lr', type=float, default=0.1, help='initial learning rate')
+    parser.add_argument('-dis_lambda', type=float, default=1, help='dis_lambda loss rate')
     parser.add_argument('-resume', action='store_true', default=False, help='resume training')
     args = parser.parse_args()
 
     net = get_network(args)
-
+    print(args)
     #data preprocessing:
     cifar100_training_loader = get_training_dataloader(
         settings.CIFAR100_TRAIN_MEAN,
@@ -145,6 +176,7 @@ if __name__ == '__main__':
         batch_size=args.b,
         shuffle=True
     )
+    dis_criterion = torch.nn.L1Loss()
 
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
@@ -209,9 +241,10 @@ if __name__ == '__main__':
             if epoch <= resume_epoch:
                 continue
 
-        train(epoch)
+        train(epoch, dis_lambda=args.dis_lambda)
         acc = eval_training(epoch)
-
+        eval_training(epoch, output_idx=1)
+        eval_training(epoch, output_idx=2)
         #start to save best performance model after learning rate decay to 0.01
         if epoch > settings.MILESTONES[1] and best_acc < acc:
             weights_path = checkpoint_path.format(net=args.net, epoch=epoch, type='best')
