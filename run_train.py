29ea9c34d2d49bcb5565049ee5f643a53ab2a8ad
Yif-Yang <yif_yang@qq.com>
2021-11-25 18:30:28 +0800

    new baseline

---
+++ run_train.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+parser = argparse.ArgumentParser()
+parser.add_argument('-net', type=str, required=True, help='net type')
+parser.add_argument('-work_dir', type=str, default='./work_dir', help='dir name')
+parser.add_argument('-exp_name', type=str, default='baseline', help='exp_name ')
+parser.add_argument('-runs', type=str, default='1', help='exp_name ')
+parser.add_argument('-blob_dir', type=str, default='/blob_aml_k8s_skt_australiav100data/output/ensemble/cifar100',
+                    help='dir name')
+parser.add_argument('-gpu',  type=int, default=0, help='batch size for dataloader')
+parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
+parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
+parser.add_argument('-lr', type=float, default=0.1, help='initial learning rate')
+parser.add_argument('-resume', action='store_true', default=False, help='resume training')
+parser.add_argument('-seed', type=int, default=-1, metavar='S', help='random seed (default: 1)')
+
+args = parser.parse_args()
+if not os.path.exists('./work_dir'):
+      os.mkdir('./work_dir')
+exp_name = f"{args.exp_name}_{args.net}_{f'_seed_{args.seed}' if args.seed > 0 else ''}_run-{args.runs}"
+cmd = f"CUDA_VISIBLE_DEVICES={args.gpu} " \
+      "nohup python train.py " \
+      f"-net {args.net} " \
+      f"{'-loss_aux_single ' if args.loss_aux_single else ''}" \
+      f"{'-loss_aux_ensemble ' if args.loss_aux_ensemble else ''}" \
+      f"-aux_dis_lambda={args.aux_dis_lambda} " \
+      f"-work_dir={os.path.join(args.work_dir, exp_name)} " \
+      f"-blob_dir={args.blob_dir} " \
+      f"-seed={args.seed} " \
+      f"> {exp_name}.out &"
+print(cmd)
+os.system(cmd)
--- train.py
+++ train.py
@@ -21,19 +21,44 @@ import torchvision.transforms as transforms
 
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-
+import shutil
 from conf import settings
 from utils import get_network, get_training_dataloader, get_test_dataloader, WarmUpLR, \
     most_recent_folder, most_recent_weights, last_epoch, best_acc_weights
+from torch.nn.functional import softmax
+import logging
 
+def get_logger(file_path):
+    """ Make python logger """
+    # [!] Since tensorboardX use default logger (e.g. logging.info()), we should use custom logger
+    logger = logging.getLogger('USNet')
+    log_format = '%(asctime)s | %(message)s'
+    formatter = logging.Formatter(log_format, datefmt='%m/%d %I:%M:%S %p')
+    file_handler = logging.FileHandler(file_path)
+    file_handler.setFormatter(formatter)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    logger.setLevel(logging.INFO)
+
+    return logger
 def train(epoch):
 
     start = time.time()
     net.train()
-    train_loss = 0
-    correct = 0
-    total = 0
+    Batch_time = AverageMeter('batch_time', ':6.3f')
+    Data_time = AverageMeter('Data time', ':6.3f')
+    Train_loss = AverageMeter('Train_loss', ':6.3f')
+    Acc1 = AverageMeter('Acc1@1', ':6.2f')
+    progress = ProgressMeter(
+        len(cifar100_training_loader),
+        [Batch_time, Data_time, Train_loss, Acc1], prefix="Epoch: [{}]".format(epoch))
+    end = time.time()
+    logger.info(f"epoch: {epoch} LR: {optimizer.param_groups[0]['lr']}")
     for batch_index, (images, labels) in enumerate(cifar100_training_loader):
+        Data_time.update(time.time() - end)
 
         if args.gpu:
             labels = labels.cuda()
@@ -45,11 +70,9 @@ def train(epoch):
         loss.backward()
         optimizer.step()
 
-        train_loss += loss.item()
-        _, predicted = outputs.max(1)
-        total += labels.size(0)
-        correct += predicted.eq(labels).sum().item()
-
+        Train_loss.update(loss.item(), labels.size(0))
+        acc_1 = accuracy(outputs, labels)[0]
+        Acc1.update(acc_1[0], labels.size(0))
 
         n_iter = (epoch - 1) * len(cifar100_training_loader) + batch_index + 1
 
@@ -60,21 +83,12 @@ def train(epoch):
             if 'bias' in name:
                 writer.add_scalar('LastLayerGradients/grad_norm2_bias', para.grad.norm(), n_iter)
 
-        # print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
-        #     loss.item(),
-        #     optimizer.param_groups[0]['lr'],
-        #     epoch=epoch,
-        #     trained_samples=batch_index * args.b + len(images),
-        #     total_samples=len(cifar100_training_loader.dataset)
-        # ))
-        if batch_index % 100 == 0:
-            print(batch_index, len(cifar100_training_loader), 'LR: %.4f | Train Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (optimizer.param_groups[0]['lr'], loss / (batch_index + 1), 100. * correct / total, correct,
-                            total) )
-
         #update training loss for each iteration
         writer.add_scalar('Train/loss', loss.item(), n_iter)
-
+        Batch_time.update(time.time() - end)
+        end = time.time()
+        if batch_index % args.print_freq == 0:
+            progress.display(batch_index)
         if epoch <= args.warm:
             warmup_scheduler.step()
 
@@ -85,18 +99,23 @@ def train(epoch):
 
     finish = time.time()
 
-    print('epoch {} training time consumed: {:.2f}s'.format(epoch, finish - start))
+    logger.info('epoch {} training time consumed: {:.2f}s'.format(epoch, finish - start))
 
 @torch.no_grad()
 def eval_training(epoch=0, tb=True):
 
     start = time.time()
     net.eval()
-
-    test_loss = 0.0 # cost function error
-    correct = 0.0
-
+    Batch_time = AverageMeter('batch_time', ':6.3f')
+    Data_time = AverageMeter('Data time', ':6.3f')
+    Test_loss = AverageMeter('Train_loss', ':6.3f')
+    Acc1 = AverageMeter('Acc1@1', ':6.2f')
+    progress = ProgressMeter(
+        len(cifar100_training_loader),
+        [Batch_time, Data_time, Test_loss, Acc1], prefix="Test Epoch: [{}]".format(epoch))
+    end = time.time()
     for (images, labels) in cifar100_test_loader:
+        Data_time.update(time.time() - end)
 
         if args.gpu:
             images = images.cuda()
@@ -105,44 +124,108 @@ def eval_training(epoch=0, tb=True):
         outputs = net(images)
         loss = loss_function(outputs, labels)
 
-        test_loss += loss.item()
-        _, preds = outputs.max(1)
-        correct += preds.eq(labels).sum()
+        acc_1 = accuracy(outputs, labels)[0]
+        Test_loss.update(loss.item(), labels.size(0))
 
-    finish = time.time()
-    # if args.gpu:
-    #     print('GPU INFO.....')
-    #     print(torch.cuda.memory_summary(), end='')
-    print('Evaluating Network.....')
-    print('Test set: Epoch: {}, Average loss: {:.4f}, Accuracy: {:.4f}, Time consumed:{:.2f}s'.format(
-        epoch,
-        test_loss / len(cifar100_test_loader.dataset),
-        correct.float() / len(cifar100_test_loader.dataset),
-        finish - start
-    ))
-    print()
+        Acc1.update(acc_1[0], labels.size(0))
+        Batch_time.update(time.time() - end)
+    logger.info(progress.display_avg())
 
-    #add informations to tensorboard
-    if tb:
-        writer.add_scalar('Test/Average loss', test_loss / len(cifar100_test_loader.dataset), epoch)
-        writer.add_scalar('Test/Accuracy', correct.float() / len(cifar100_test_loader.dataset), epoch)
+    return Acc1.avg
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
 
-    return correct.float() / len(cifar100_test_loader.dataset)
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        # print('\t'.join(entries))
+        logger.info('\t'.join(entries))
+
+    def display_avg(self):
+        entries = [self.prefix ]
+        entries += [f"{meter.name}:{meter.avg:6.3f}" for meter in self.meters]
+        # print('\t'.join(entries))
+        logger.info('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-net', type=str, required=True, help='net type')
+    parser.add_argument('-work_dir', type=str, default='./work_dir', help='dir name')
+    parser.add_argument('-blob_dir', type=str, default='/blob_aml_k8s_skt_australiav100data/output/ensemble/cifar100', help='dir name')
     parser.add_argument('-gpu', action='store_true', default=True, help='use gpu or not')
     parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
     parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
+    parser.add_argument('-print_freq', type=int, default=100, help='warm up training phase')
     parser.add_argument('-lr', type=float, default=0.1, help='initial learning rate')
     parser.add_argument('-resume', action='store_true', default=False, help='resume training')
-    args = parser.parse_args()
+    parser.add_argument('-seed', type=int, default=-1, metavar='S', help='random seed (default: 1)')
 
+    args = parser.parse_args()
+    torch.backends.cudnn.benchmark = True
+    if args.seed > 0:
+        print(f'set seed {args.seed}')
+        torch.manual_seed(args.seed)
+        torch.cuda.manual_seed(args.seed)
+        np.random.seed(args.seed)
+    else:
+        print('not set seed')
     net = get_network(args)
-    print(args)
-    print(net)
+    if not os.path.exists(args.work_dir):
+        os.mkdir(args.work_dir)
+    logger = get_logger(os.path.join(args.work_dir, 'train.log'))
+    logger.info(args)
+    logger.info(net)
+    settings.CHECKPOINT_PATH = os.path.join(args.work_dir, 'ckpt')
+    settings.LOG_DIR = os.path.join(args.work_dir, 'pb')
     #data preprocessing:
     cifar100_training_loader = get_training_dataloader(
         settings.CIFAR100_TRAIN_MEAN,
@@ -195,25 +278,25 @@ if __name__ == '__main__':
     checkpoint_path = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
 
     best_acc = 0.0
+    best_ep = 0
     if args.resume:
         best_weights = best_acc_weights(os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder))
         if best_weights:
             weights_path = os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder, best_weights)
-            print('found best acc weights file:{}'.format(weights_path))
-            print('load best training file to test acc...')
+            logger.info('found best acc weights file:{}'.format(weights_path))
+            logger.info('load best training file to test acc...')
             net.load_state_dict(torch.load(weights_path))
             best_acc = eval_training(tb=False)
-            print('best acc is {:0.2f}'.format(best_acc))
+            logger.info('best acc is {:0.2f}'.format(best_acc))
 
         recent_weights_file = most_recent_weights(os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder))
         if not recent_weights_file:
             raise Exception('no recent weights file were found')
         weights_path = os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder, recent_weights_file)
-        print('loading weights file {} to resume training.....'.format(weights_path))
+        logger.info('loading weights file {} to resume training.....'.format(weights_path))
         net.load_state_dict(torch.load(weights_path))
 
         resume_epoch = last_epoch(os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder))
-
 
     for epoch in range(1, settings.EPOCH + 1):
         if epoch > args.warm:
@@ -225,18 +308,22 @@ if __name__ == '__main__':
 
         train(epoch)
         acc = eval_training(epoch)
-
+        if best_acc < acc:
+            best_acc = acc
+            best_ep = epoch
+        logger.info(f'epoch({epoch}): best acc-{best_acc:6.3f} from ep {best_ep}')
         #start to save best performance model after learning rate decay to 0.01
         if epoch > settings.MILESTONES[1] and best_acc < acc:
             weights_path = checkpoint_path.format(net=args.net, epoch=epoch, type='best')
-            print('saving weights file to {}'.format(weights_path))
+            logger.info('saving weights file to {}'.format(weights_path))
             torch.save(net.state_dict(), weights_path)
             best_acc = acc
             continue
 
         if not epoch % settings.SAVE_EPOCH:
             weights_path = checkpoint_path.format(net=args.net, epoch=epoch, type='regular')
-            print('saving weights file to {}'.format(weights_path))
+            logger.info('saving weights file to {}'.format(weights_path))
             torch.save(net.state_dict(), weights_path)
-
+    if os.path.exists(args.blob_dir):
+        shutil.copytree(args.work_dir, args.blob_dir)
     writer.close()
