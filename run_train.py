d0a4e75212e9c1f2d618cad3efaff2cf4e645cfe
Yif-Yang <yif_yang@qq.com>
2022-01-14 02:06:17 +0800

    add eval

--- run_train.py
+++ run_train.py
@@ -3,7 +3,7 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument('-net', type=str, default='resnet18_new', help='net type')
 parser.add_argument('-work_dir', type=str, default='./work_dir', help='dir name')
-parser.add_argument('-exp_name', type=str, default='hm_nodis', help='exp_name ')
+parser.add_argument('-exp_name', type=str, default='hm', help='exp_name ')
 parser.add_argument('-runs', type=str, default='fix_mask', help='exp_name ')
 parser.add_argument('-blob_dir', type=str, default='/blob_aml_k8s_skt_yif_resrchvc4data/output/ensemble/cifar100',
                     help='dir name')
@@ -23,11 +23,13 @@ parser.add_argument('-distillation-type', default='soft', choices=['none', 'soft
 parser.add_argument('-distillation-alpha', default=0.5, type=float, help="")
 parser.add_argument('-distillation-tau', default=1.0, type=float, help="")
 parser.add_argument('-div-tau', default=1.0, type=float, help="")
+parser.add_argument('-hm_add_dis', action='store_true', default=False, help='add_dis_w training')
+
 args = parser.parse_args()
 if not os.path.exists('./work_dir'):
       os.mkdir('./work_dir')
 exp_name = f"{args.exp_name}_{args.net}_nEns-{args.n_estimators}_lr{args.lr}_dis-length-{args.aux_dis_lambda}_hm-value-{args.hm_value}_" \
-           f"{f'nesterov_' if args.nesterov else ''}{f'add_cls_w_' if args.add_cls_w else ''}{f'add_dis_w_' if args.add_dis_w else ''}" \
+           f"{f'nesterov_' if args.nesterov else ''}{f'add_cls_w_' if args.add_cls_w else ''}{f'add_dis_w_' if args.add_dis_w else ''}{f'hm_add_dis_' if args.hm_add_dis else ''}" \
            f"distillation-type-{args.distillation_type}_distillation-alpha-{args.distillation_alpha}_distillation-tau-{args.distillation_tau}_" \
            f"_div-tau-{args.div_tau}_{f'seed_{args.seed}' if args.seed > -1 else ''}_run-{args.runs}"
 cmd = f"CUDA_VISIBLE_DEVICES={args.gpu} " \
@@ -45,6 +47,7 @@ cmd = f"CUDA_VISIBLE_DEVICES={args.gpu} " \
       f"-div-tau={args.div_tau} " \
       f"{f'-add_cls_w ' if args.add_cls_w else ''}" \
       f"{f'-add_dis_w ' if args.add_dis_w else ''}" \
+      f"{f'-hm_add_dis ' if args.hm_add_dis else ''}" \
       f"-blob_dir={args.blob_dir} " \
       f"-seed={args.seed} " \
       f"> {exp_name}.out &"
